00fae1ac31fb94859149ed133f605dca5def87c2
thenimashahi <60892441+thenimashahi@users.noreply.github.com>
2020-04-23 16:55:40 -0400

    initialized the Noisemaker Node Prokect
    initialized the noisemaker project as a node project and imported the dependencies and loaded static resources

---
+++ index.js
@@ -0,0 +1,35 @@
+//Node Entry Point File 
+
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var app = express();
+
+//GET , POST, PUT, DELETE
+//(READ, CREATE, UPDATE, DELETE )
+
+//importing handlebars into the app
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
+
+//load static resources
+app.use(express.static("public"));
+
+
+//home route
+app.get('/', (req, res) => {
+
+
+    res.render('home');
+
+});
+
+
+
+//Opening the server
+const PORT = 3000;
+app.listen(PORT , ()=>
+{
+        console.log(`Web application is up and running. `);
+});
